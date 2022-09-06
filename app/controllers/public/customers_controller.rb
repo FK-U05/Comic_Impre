abb51031d631c407ded9c8aad0aaf46e6e37b3c0
EC2 Default User <ec2-user@ip-172-31-16-113.ap-northeast-1.compute.internal>
2022-09-06 07:10:24 +0000

    ユーザー画像を表示する機能を追加、gem追加記述

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -30,7 +30,7 @@ class Public::CustomersController < ApplicationController
 private
 
   def customer_params
-    params.require(:customer).permit(:name)
+    params.require(:customer).permit(:name, :profile_image)
   end
 
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,7 +4,17 @@ class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_one_attached :profile_image
+
   has_many :comics, dependent: :destroy
+
+  def get_profile_image(width,height)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpeg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize_to_fit: [width, height]).processed
+  end
 
 #ゲストログイン時のcustomer情報
   def self.guest
