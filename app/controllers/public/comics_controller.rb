e0d59649a8ee3522ba2a2c17b429f87d5711cd7d
EC2 Default User <ec2-user@ip-172-31-16-113.ap-northeast-1.compute.internal>
2022-09-07 07:02:19 +0000

    投稿機能作成中

--- app/controllers/public/comics_controller.rb
+++ app/controllers/public/comics_controller.rb
@@ -1,18 +1,28 @@
 class Public::ComicsController < ApplicationController
 
   def index
+    @comics = Comic.all
   end
 
   def new
+    @comic = Comic.new
   end
 
   def create
+    @comic = Comic.new(comic_params)
+    @comic.customer_id = current_customer.id
+    if @comic.save
+    redirect_to public_comic_path(@comic)
+    end
   end
 
   def check
+    @comic = Comic.new(comic_params)
+    @comic.customer_id = current_customer.id
   end
 
   def show
+    @comic = Comic.find(params[:id])
   end
 
   def edit
@@ -22,6 +32,11 @@ class Public::ComicsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def comic_params
+    params.require(:comic).permit(:company_id, :title, :body, :name, :release_date)
   end
 
 end
