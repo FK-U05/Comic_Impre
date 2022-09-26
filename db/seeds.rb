b222de5cacaf378e21704cf98c0f6b1bf0d8aae3
EC2 Default User <ec2-user@ip-172-31-16-113.ap-northeast-1.compute.internal>
2022-09-26 13:03:01 +0000

    テストデータ挿入

--- db/seeds.rb
+++ db/seeds.rb
@@ -35,3 +35,50 @@ Admin.create!(
     email: 'admin@admin',
     password: '123456',
 )
+
+#テストデータ
+Customer.create!(
+    name: 'customer1',
+    email: 'customer1@customer1',
+    password: 'customer1',
+    password_confirmation: 'customer1',
+    profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open(Rails.root.join("./app/assets/images/sample_customer1.jpg")),filename: "sample_customer1.jpg"))
+
+Customer.create!(
+    name: 'customer2',
+    email: 'customer2@customer2',
+    password: 'customer2',
+    password_confirmation: 'customer2',
+    profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open(Rails.root.join("./app/assets/images/sample_customer2.jpg")),filename: "sample_customer2.jpg"))
+
+Customer.create!(
+    name: 'customer3',
+    email: 'customer3@customer3',
+    password: 'customer3',
+    password_confirmation: 'customer3',
+    profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open(Rails.root.join("./app/assets/images/sample_customer3.jpg")),filename: "sample_customer3.jpg"))
+
+Comic.create!(
+    customer_id: 1,
+    company: "集英社",
+    title: "ハイキュー！！",
+    body: "高校生バレーの漫画です。バレーのルールを全く知らない人にもわかりやすくルールと魅力を解説してくれているので、すぐ物語の魅力に引き込まれます。誰が読んでも楽しめるバレー漫画だと思います。",
+    name: "古舘春一",
+    release_date: '2012-06-09')
+
+Comic.create!(
+    customer_id: 2,
+    company: "KADOKAWA",
+    title: "ダンジョン飯",
+    body: "ドラゴンに食べられてしまった妹をすぐに助けに行くため、迷宮の中にいる魔物を食べて行くことにしたパーティーの話です。現実にない食糧を調理していく様は不思議と見入ってしまいます。",
+    name: "九井諒子",
+    release_date: '2015-01-27')
+
+Comic.create!(
+    customer_id: 3,
+    company: "白泉社",
+    title: "赤髪の白雪姫",
+    body: "赤い髪を持つ薬剤師の主人公・白雪が、ある日隣の国の王子様と出会う王宮ファンタジーものです。主人公の白雪がとてもかっこいい女の子なので見ていてとても爽快です！",
+    name: "あきづき空太",
+    release_date: '2007-12-05')
+
